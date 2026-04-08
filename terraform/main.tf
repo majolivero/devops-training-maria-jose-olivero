42d9b086a84f06991ffd589e061479c415eea0b6
Maria-Jose-Olivero <support@quind.io>
2026-04-08 17:00:06 -0500

    terraform: descomentar recursos, importar en state y actualizar tfvars con valores reales

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,14 @@ data "azurerm_resource_group" "rg" {
 }
 
 # Virtual Network - YA EXISTE EN AZURE
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = var.vnet_name
-#   address_space       = var.address_space
-#   location            = data.azurerm_resource_group.rg.location
-#   resource_group_name = data.azurerm_resource_group.rg.name
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
 
-# Subnet
+# Subnet - DEPENDE DE VNET COMENTADO
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = data.azurerm_resource_group.rg.name        #Usamos un RG ya existente (mi RG)
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = var.subnet_address_prefix
 }
 
-# Network Interface
+# Network Interface - DEPENDE DE SUBNET Y PUBLIC_IP COMENTADOS
 resource "azurerm_network_interface" "nic" {
   name                = var.nic_name
   location            = data.azurerm_resource_group.rg.location      #Network Interface usa la ubicación del RG existente
@@ -35,76 +35,76 @@ resource "azurerm_network_interface" "nic" {
 }
 
 # Public IP Address - YA EXISTE EN AZURE
-# resource "azurerm_public_ip" "public_ip" {
-#   name                = var.public_ip_name
-#   location            = data.azurerm_resource_group.rg.location     #Pasa lo mismo que en linea 26 y 27
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   allocation_method   = "Static"
-# }
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = data.azurerm_resource_group.rg.location     #Pasa lo mismo que en linea 26 y 27
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
 
 # Network Security Group
 # Network Security Group - YA EXISTE EN AZURE
-# resource "azurerm_network_security_group" "nsg" {
-#   name                = var.nsg_name
-#   location            = data.azurerm_resource_group.rg.location    #Pasa lo mismo que en linea 26 y 27
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#
-#   security_rule {
-#     name                       = "AllowSSH"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-#
-#   security_rule {
-#     name                       = "AllowHTTP"
-#     priority                   = 101
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "80"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-#
-#   security_rule {
-#     name                       = "AllowHTTPS"
-#     priority                   = 102
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "443"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-#
-#   security_rule {
-#     name                       = "AllowAppPort"
-#     priority                   = 103
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "8080"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-# }
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = data.azurerm_resource_group.rg.location    #Pasa lo mismo que en linea 26 y 27
+  resource_group_name = data.azurerm_resource_group.rg.name
 
-# Associate NSG with Subnet
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowAppPort"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Subnet - DEPENDE DE SUBNET Y NSG COMENTADOS
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# Virtual Machine
+# Virtual Machine - DEPENDE DE NIC COMENTADO
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   location            = data.azurerm_resource_group.rg.location     #VM usa info del RG existente, porque la VM se va a crear aquí
@@ -137,35 +137,35 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 # 1. Azure Container Registry (ACR) - YA EXISTE EN AZURE
-# resource "azurerm_container_registry" "acr" {
-#   name                = var.acr_name
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   location            = data.azurerm_resource_group.rg.location
-#   sku                 = "Basic"
-#   admin_enabled       = true
-# }
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
 
 # 2. Azure Kubernetes Service (AKS) - YA EXISTE EN AZURE
-# resource "azurerm_kubernetes_cluster" "aks" {
-#   name                = var.aks_cluster_name
-#   location            = data.azurerm_resource_group.rg.location
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   dns_prefix          = var.aks_dns_prefix
-#
-#   default_node_pool {
-#     name       = "default"
-#     node_count = var.aks_node_count
-#     vm_size    = var.aks_node_vm_size
-#   }
-#
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#
-#   tags = {
-#     Environment = "Dev"
-#   }
-# }
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  dns_prefix          = var.aks_dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.aks_node_count
+    vm_size    = var.aks_node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
 
 # 3. Permiso para que AKS pueda jalar imágenes del ACR (Role Assignment)
 #resource "azurerm_role_assignment" "aks_acr_pull" {
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -3,21 +3,22 @@
 
 
 # VM Configuration
-resource_group_name = "#{RESOURCE_GROUP}#"
-location            = "#{LOCATION}#"
-vnet_name           = "#{VNET_NAME}#"
-subnet_name         = "#{SUBNET_NAME}#"
-nic_name            = "#{NIC_NAME}#"
-public_ip_name      = "#{PUBLIC_IP_NAME}#"
-nsg_name            = "#{NSG_NAME}#"
-vm_name             = "#{VM_NAME}#"
-vm_size             = "#{VM_SIZE}#"
-admin_username      = "#{ADMIN_USERNAME}#"
-public_key_path     = "#{PUBLIC_KEY_PATH}#"
-acr_name            = "#{ACR_NAME}#"
-aks_cluster_name    = "#{AKS_CLUSTER_NAME}#"
-aks_dns_prefix      = "#{AKS_DNS_PREFIX}#"
-aks_node_count      = #{AKS_NODE_COUNT}#
-aks_node_vm_size    = "#{AKS_NODE_VM_SIZE}#"
+resource_group_name = "resource-group-maria-jose-olivero"
+location            = "East US"
+vnet_name           = "vnet-calculadora"
+subnet_name         = "subnet-calculadora"
+nic_name            = "nic-calculadora"
+public_ip_name      = "pip-calculadora"
+nsg_name            = "nsg-calculadora"
+vm_name             = "vm-calculadora"
+vm_size             = "Standard_B2s"
+admin_username      = "azureuser"
+public_key_path     = "~/.ssh/id_rsa.pub"
+acr_name            = "acrmajolivero1772830059"
+aks_cluster_name    = "aks-calculadora-majo"
+aks_dns_prefix      = "aks-calc"
+aks_node_count      = 1
+aks_node_vm_size    = "Standard_B2s"
 address_space       = ["10.0.0.0/16"]           #Rango total de direcciones IP que el Virtual Network puede usar. Tiene 65536 direcciones IP disponibles.
 subnet_address_prefix = ["10.0.1.0/24"]         #Es un sub-rango dentro del Virtual Network que asignas a una Subnet específica.
+ssh_public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCrfSmivyFF6T2p/r5uDlAo6kJTLPmsiNhWbureG9mBtUI+yg0xXLpqXh66wqeSOT0DZ2PQlptkGqlRM5Qqs1ZjM59qxXfXnx/7bYK8xBbqP929zgFjd3P+mq/kACfiBw9pkck0+ehdfJ0vGweC+pqzqoBRmcATqSUrnIRK++3bKcD03Fd/cEDjasRBuKN4mDzVihiE9pa6+cd09bsU+12ry7mRcTlZ+ZfBEaZ3rf2BFN5cphK+BDY8jHrW09HSz8xLs8CZJSBwaqI3Iw41MFnyrangxRZVJIHZsnVlanS0J+r4IlPTlZ6jNmLwEzguWwmyp1dw4yiR7vz4YusOxtHe9Pqs4RozA3CP+TaM3ceofvOzKsoC88hvZdBojT9rJP33qV/Ia8LJu0LkleX4oVIkkvZEjyJoDpt678u/72SLwsDXF0TCODKbxK6fXryl8xjN01WmyYidmOkukkj0m9wPqSd1HcSO7K0hhPb0QgnCOFSI+IZ7f3Q+5YGRV6B8fd3CPFhWtW2GagekeOrwiygJt6aRoRk7UWw04Gx3D31FjwdgAi0TYOzqSa6Xc+oxhvzi3GO3AYkX2V/yU/DNAFuJXxC/WK7i1gAGZl5EcWVr+rrZJnQ/MbDUrdYf7/qPsQl9niLX2+9y5qLRlOkDeHNbwnfwHzVUwqc7CxJzWcNz6w== azureuser@calculadora"
